5ce60c9f5f74eead6f577535364aaf122cb32d6d
Heidi Housten <14905544+housten@users.noreply.github.com>
2025-10-15 17:13:01 +0200

    allow for space seperators

--- MetricsApi/Authorization/ScopeOrRoleHandler.cs
+++ MetricsApi/Authorization/ScopeOrRoleHandler.cs
@@ -8,8 +8,8 @@ public class ScopeOrRoleHandler : AuthorizationHandler<ScopeOrRoleRequirement>
     protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ScopeOrRoleRequirement requirement)
     {
         // Check for the 'scp' claim for delegated permissions
-        var scopeClaim = context.User.FindFirst(c => c.Type == "http://schemas.microsoft.com/identity/claims/scope");
-        if (scopeClaim != null && scopeClaim.Value.Split(' ').Contains(requirement.Scope))
+        var scopeClaim = context.User.FindFirst(c => c.Type == "http://schemas.microsoft.com/identity/claims/scope" || c.Type == "scp");
+        if (scopeClaim != null && scopeClaim.Value.Split(' ', StringSplitOptions.RemoveEmptyEntries).Contains(requirement.Scope))
         {
             context.Succeed(requirement);
             return Task.CompletedTask;
@@ -17,7 +17,7 @@ public class ScopeOrRoleHandler : AuthorizationHandler<ScopeOrRoleRequirement>
 
         // Check for the 'roles' claim for application permissions
         var roleClaim = context.User.FindFirst(c => c.Type == ClaimTypes.Role || c.Type == "roles");
-        if (roleClaim != null && roleClaim.Value.Split(' ').Contains(requirement.Role))
+        if (roleClaim != null && roleClaim.Value.Split(' ', StringSplitOptions.RemoveEmptyEntries).Contains(requirement.Role))
         {
             context.Succeed(requirement);
             return Task.CompletedTask;
